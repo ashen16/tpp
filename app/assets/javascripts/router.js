f42336bc20fdb2aba85ac58f4e465c7fdf3273fa
James Chuang <jameschuang13@gmail.com>
2014-02-11 14:50:31 -0800

    Revert "add basic wireframe for home page"
    This reverts commit c80e7febe18e5487750386f204dd3ffdc519bc58.

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,6 +1,5 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 Tpp.Router.map(function() {
-  this.resource('posts');
-  this.resource('reviews');
+  // this.resource('posts');
 });
