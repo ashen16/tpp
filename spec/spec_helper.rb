f315bcdcbd4aeb1842f3445ffe2f11f58a451bca
James Chuang <jameschuang13@gmail.com>
2014-02-26 23:46:48 -0800

    set I18n validation to false

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'simplecov'
+SimpleCov.start
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -39,6 +41,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  I18n.enforce_available_locales = false
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
