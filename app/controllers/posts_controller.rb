ec3a7c3ac24453885977521a5e75f504f5c75886
James Chuang <jameschuang13@gmail.com>
2014-03-20 21:37:37 -0700

    update views and nav

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,10 @@ class PostsController < ApplicationController
 	def index
 		if params[:tag]
     	@posts = policy_scope(Post).order('created_at DESC').tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+    	@post = @posts.first
   	else
     	@posts = policy_scope(Post).order('created_at DESC').paginate(:page => params[:page], :per_page => 5)
+    	@post = @posts.first
   	end
 	end
 
